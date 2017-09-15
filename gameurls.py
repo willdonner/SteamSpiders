85c51f047a0730616f2787755ff89e9a17105781
willdonner <765019300@qq.com>
2017-09-15 17:11:31 +0800

    basecode

---
+++ gameurls.py
@@ -0,0 +1,6 @@
+import re
+import config
+import utils
+from scrapy.spider import Spider
+from scrapy import Request
+from scrapy.selector import Selector
---
+++ sqlhelper.py
@@ -0,0 +1,22 @@
+from distutils import command
+
+import logging2
+import mysql.connector
+import utils
+import config
+from singleton import Singleton
+
+class Sqlhelper(Singleton):
+    def __init__(self):
+        self.database_name = config.database_name
+        self.inti()
+
+    def inti(self):
+        self.database = mysql.connector.connect(**config.database_config)
+        self.cursor = self.database.cursor()
+        self.create_database()
+        self.database.database = self.database_name
+
+    def create_database(self):
+        try:
+            command =   'CREATE DATABASE IF NO EXISTS %s DEFAULT CHARACTER '
